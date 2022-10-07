fbc56e8c4ff3c1d74d7690784d91ef09e967fb59
Bruna Girão <brunagirao.s@gmail.com>
2022-10-07 10:54:03 -0300

    update build.sh with sfdx cli comands

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,13 @@
 set -e
 
 # Create scratch org
-sfdx force:org:create -f config/project-scratch-def.json -a ebury_test5 -s
+sfdx force:org:create -f config/project-scratch-def.json --durationdays 30 -a trade-lwc-scratch
+
+# Set defaultusername scratch org
+sfdx force:config:set defaultusername=trade-lwc-scratch
+
+# Open scratch org
+sfdx force:org:open
 
 # Push changes
 sfdx force:source:push
