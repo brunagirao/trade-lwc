4cf2def4699a52b4fa13adbe81eef17c514c4f22
Bruna Girão <brunagirao.s@gmail.com>
2022-10-09 16:07:35 -0300

    add wire to get values to currencies comboboxes  n' create handle methods to get currency value selected

--- force-app/main/default/lwc/newTrade/newTrade.js
+++ force-app/main/default/lwc/newTrade/newTrade.js
@@ -1,4 +1,41 @@
-import {LightningElement} from 'lwc';
+import { LightningElement, api, track, wire } from 'lwc';
+
+import { getPicklistValues } from 'lightning/uiObjectInfoApi';
+import CURRENCIES from '@salesforce/schema/Trade__c.Sell_Currency__c';
+import { getObjectInfo  } from 'lightning/uiObjectInfoApi';
+import TRADE_OBJECT from '@salesforce/schema/Trade__c';
 
 export default class NewTrade extends LightningElement {
+    //TRACKS
+    @track currencyOptionList  = [];
+    @track sellCurrencyOptions = [];
+    @track buyCurrencyOptions  = [];
+    
+
+    //WIRES
+    @wire(getObjectInfo, { objectApiName: TRADE_OBJECT })
+    tradeInfo;
+
+    @wire(getPicklistValues, { recordTypeId: '$tradeInfo.data.defaultRecordTypeId', fieldApiName: CURRENCIES })
+    currencyOptions;
+    
+
+    connectedCallback() {
+    }
+
+    //HANDLES
+    handleSelectedSellCurrency(element) {
+        console.log('handleSelectedSellCurrency');
+        
+		let sellCurrency = element.target.value;
+		console.log('Sell Currency: ', sellCurrency);
+    }
+    
+    handleSelectedBuyCurrency(element) {
+        console.log('handleSelectedBuyCurrency');
+        
+		let buyCurrency = element.target.value;
+		console.log('Buy Currency: ', buyCurrency);
+	}
+
 }
