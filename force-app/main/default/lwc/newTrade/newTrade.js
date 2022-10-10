32248c62bb364ef91ba144ebbb2fbbe8304938e8
Bruna Girão <brunagirao.s@gmail.com>
2022-10-10 07:16:03 -0300

    add logic to getRate

--- force-app/main/default/lwc/newTrade/newTrade.js
+++ force-app/main/default/lwc/newTrade/newTrade.js
@@ -1,41 +1,60 @@
 import { LightningElement, api, track, wire } from 'lwc';
 
 import { getPicklistValues } from 'lightning/uiObjectInfoApi';
-import CURRENCIES from '@salesforce/schema/Trade__c.Sell_Currency__c';
-import { getObjectInfo  } from 'lightning/uiObjectInfoApi';
-import TRADE_OBJECT from '@salesforce/schema/Trade__c';
+import { getObjectInfo }     from 'lightning/uiObjectInfoApi';
+
+import CURRENCIES            from '@salesforce/schema/Trade__c.Sell_Currency__c';
+import TRADE_OBJECT          from '@salesforce/schema/Trade__c';
+
+import getRate from '@salesforce/apex/NewTradeController.getRate';
 
 export default class NewTrade extends LightningElement {
-    //TRACKS
-    @track currencyOptionList  = [];
-    @track sellCurrencyOptions = [];
-    @track buyCurrencyOptions  = [];
-    
 
+    //TRACKS
+    @track sellCurrencyOptions  = [];
+    @track buyCurrencyOptions   = [];
+    @track sellCurrencySelected;
+    @track buyCurrencySelected;
+    @track rate;
+    
     //WIRES
     @wire(getObjectInfo, { objectApiName: TRADE_OBJECT })
     tradeInfo;
 
     @wire(getPicklistValues, { recordTypeId: '$tradeInfo.data.defaultRecordTypeId', fieldApiName: CURRENCIES })
     currencyOptions;
-    
 
-    connectedCallback() {
-    }
+    connectedCallback() {}
 
     //HANDLES
     handleSelectedSellCurrency(element) {
         console.log('handleSelectedSellCurrency');
         
-		let sellCurrency = element.target.value;
-		console.log('Sell Currency: ', sellCurrency);
+        this.sellCurrencySelected = element.target.value;
+        console.log('Sell Currency: ', this.sellCurrencySelected);
+        if (this.sellCurrencySelected != undefined || this.sellCurrencySelected != null) {
+           this.getRate(); 
+        };
     }
     
     handleSelectedBuyCurrency(element) {
         console.log('handleSelectedBuyCurrency');
         
-		let buyCurrency = element.target.value;
-		console.log('Buy Currency: ', buyCurrency);
-	}
+		this.buyCurrencySelected = element.target.value;
+        console.log('Buy Currency: ', this.buyCurrencySelected);
+
+        if (this.buyCurrencySelected != undefined || this.buyCurrencySelected != null) {
+           this.getRate(); 
+        }
+    }
+
+    async getRate() {
+        console.log('getRate');
+        let response  = await getRate({
+            sellCurrency : this.sellCurrencySelected,
+            buyCurrency  : this.buyCurrencySelected,
+        });
+        this.rate = response;
+    }
 
 }
