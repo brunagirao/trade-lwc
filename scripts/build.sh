05ab481793a10bab472b092927c7cb8a2e7f77fb
Bruna Girão <brunagirao.s@gmail.com>
2022-10-12 17:35:08 -0300

    update buil.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,18 +2,26 @@
 set -e
 
 # Create scratch org
+echo "1. START OF CREATION SCRATCH ORG..."
+echo " SCRATCH ORG NAME: trade-lwc-scratch"
 sfdx force:org:create -f config/project-scratch-def.json --durationdays 30 -a trade-lwc-scratch
+echo "  SCRATCH ORg CREATED!"
 
 # Set defaultusername scratch org
+echo "2. SETTING DEFAULTUSERNAME SCRATCH ORG..."
 sfdx force:config:set defaultusername=trade-lwc-scratch
-
-# Open scratch org
-sfdx force:org:open
-
-#Get Custom Settings
-sfdx force:source:retrieve -m CustomObject:EburyTradeSettings__c
+echo " SCRATCH OR CREATED!"
 
 # Push changes
-sfdx force:source:push
+echo "3. PUSHING CHANGES..."
+sfdx force:source:push -u trade-lwc-scratch
+echo " PUSHED CHANGES!"
+
+# Run Test 
+echo "4. RUNNING TESTS..."
+sfdx force:apex:test:run -n "NewTradeControllerTest" -r human 
+echo "4. RUNNED TESTS!"
+
+
 
 
